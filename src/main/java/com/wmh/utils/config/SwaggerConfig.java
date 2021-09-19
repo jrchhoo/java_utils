0f28dbe09d8f86de73c4e3f66de72946a3b1567e
wmhou <29271004+wmhou@users.noreply.github.com>
2021-09-19 10:45:39 +0800

    配置swggger和yml

---
+++ src/main/java/com/wmh/utils/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.wmh.utils.config;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @program: admin
+ * @description: swagger配置工具
+ * @author: wmhou
+ * @create: 2020-09-28 11:16
+ **/
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("utils")
+                .description("工具开发文档")
+                .termsOfServiceUrl("http://www.wmh.com")
+                .version("1.1.0")
+                .build();
+    }
+}
